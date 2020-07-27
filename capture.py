4c5d9b23d4618738a54672b33b4367d38fd7f9b6
Aashish KC <kcaashish@gmail.com>
2020-07-27 19:16:26 +0545

    adding file download with progress bar

--- capture.py
+++ capture.py
@@ -1,5 +1,7 @@
 
 import cv2
+import requests
+from tqdm import tqdm
 
 # creating an object, zero for external camera
 cap = cv2.VideoCapture(0)
@@ -10,6 +12,14 @@ if cap.isOpened():
     CHECK, frame = cap.read()
 else:
     CHECK = False
+
+url = 'https://github.com/davisking/dlib-models/raw/master/shape_predictor_68_face_landmarks.dat.bz2'
+local_filename = url.split('/')[-1]
+response = requests.get(url, stream=True)
+length = response.headers.get('content-length', 0)
+with tqdm.wrapattr(open(local_filename, 'wb'), 'write', miniters=1, desc='Downloading', total=int(length)) as fout:
+    for chunk in response.iter_content(chunk_size=4096):
+        fout.write(chunk)
 
 # # for photo capture
 # img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
